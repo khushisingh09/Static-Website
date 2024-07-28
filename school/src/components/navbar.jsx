8c91e071d816bc2fd00df153d29ce912b1e9d40e
Khushi Singh <khushi.singh092001@gmail.com>
2024-07-28 13:26:37 +0530

    updated vercel.json

--- school/src/components/navbar.jsx
+++ school/src/components/navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
+import logo from '../assets/logo.jpg';
 const Navbar = () => {
   return (
     <nav className="bg-blue-100 py-4">
       <div className="container mx-auto flex items-center justify-center space-x-4">
         <div className="flex items-center">
-          <img src="/src/assets/logo.jpg" alt="Logo" className="h-12 mr-4" />
+          <img src={logo} alt="Logo" className="h-12 mr-4" />
           <ul className="flex space-x-8">
             <li>
               <Link to="/" className="text-black hover:text-blue-600">Home</Link>
--- school/src/components/poster.jsx
+++ school/src/components/poster.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import poster from '../assets/poster.jpg';
 const PosterComponent = () => {
   return (
     <div className="w-full bg-blue-50 rounded-lg shadow-md mx-auto">
       <div className="relative w-full">
-        <img src="/src/assets/poster.jpg" alt="Dream Catchers Poster" className="w-full h-auto" />
+        <img src={poster} alt="Dream Catchers Poster" className="w-full h-auto" />
       </div>
     </div>
   );
